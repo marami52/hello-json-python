ecf2ffb2ed1c50a64b50583b9accb5caae4f0462
Ali Marami <marami@ironix.com>
2020-02-17 17:52:04 -0800

    back to simple json

--- hello-json/handler.py
+++ hello-json/handler.py
@@ -1,14 +1,6 @@
 import sys
 import json
-import requests
 
 def handle(req):
-    headers = {}
-    # request = requests.Request('GET', 'http://gateway.openfaas:8080/function/list-functions/?user=marami52', headers=headers)
-    request = requests.Request('GET', 'http://gateway11.axcel.r-brain.io/function/list-functions/?user=marami52', headers=headers)
-    prepped = request.prepare()
-    with requests.Session() as session:
-        response = session.send(prepped)
     userInput = req
-    return response.text
-    #return json.dumps({"Chakeram ostad! 11baba ... ": userInput})
+    return json.dumps({"Chakeram ostad! 11baba ... ": userInput})
