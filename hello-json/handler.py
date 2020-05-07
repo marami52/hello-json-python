9dd4db616f69762be86dc066e816e87f47dda103
Ali Marami <marami@ironix.com>
2020-05-07 15:52:28 -0700

    show input

--- hello-json/handler.py
+++ hello-json/handler.py
@@ -5,5 +5,5 @@ import os
 def handle(event, context):
     userInput = str(event.body)
     username = event.headers['Http_X_Consumer_Username']
-    return username
+    return userInput
     #return json.dumps({"Chakeram ostad! 18 baba ... ": str(req)})
